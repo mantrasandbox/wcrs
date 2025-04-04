bc40ddc02a87ccd95b4fe1f1788441a37dd75efe
Kibuuka Allan Victor <akibuuka50@gmail.com>
2025-04-04 17:33:57 +0300

    Exception method for handling validation errors

--- services/employee/src/main/java/com/wcrs/employee/exception/EmployeeExceptionHandler.java
+++ services/employee/src/main/java/com/wcrs/employee/exception/EmployeeExceptionHandler.java
@@ -2,8 +2,12 @@ package com.wcrs.employee.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class EmployeeExceptionHandler {
@@ -19,4 +23,13 @@ public class EmployeeExceptionHandler {
                 .body(new EmployeeExceptionResponse(ex.getMessage()));
     }
 
-}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), error.getDefaultMessage());
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+
+}}
