e2c929a2c1306c7a757f32eaa67da9e8a99dd6ce
Kibuuka Allan Victor <akibuuka50@gmail.com>
2025-04-04 13:55:10 +0300

    Employee create method

---
+++ services/employee/src/main/java/com/wcrs/employee/dto/EmployeeResponseDTO.java
@@ -0,0 +1,4 @@
+package com.wcrs.employee.dto;
+
+public record EmployeeResponseDTO() {
+}
--- services/employee/src/main/java/com/wcrs/employee/service/EmployeeService.java
+++ services/employee/src/main/java/com/wcrs/employee/service/EmployeeService.java
@@ -1,8 +1,13 @@
 package com.wcrs.employee.service;
 
+import com.wcrs.employee.dto.EmployeeRequestDTO;
+import com.wcrs.employee.dto.EmployeeResponseDTO;
 import com.wcrs.employee.repository.EmployeeRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,8 @@ public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+
+    public EmployeeResponseDTO createEmployee(@Valid EmployeeRequestDTO employeeRequestDTO) {
+        return null;
+    }
 }
