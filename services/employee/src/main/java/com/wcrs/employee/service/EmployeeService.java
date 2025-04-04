4a0785bc3052afa574d72ee58c6adf8ac0af44b9
tendojoshua <tendojoshua@gmail.com>
2025-04-04 12:56:43 +0300

    Added Controller, Service and Repository packages

---
+++ services/employee/src/main/java/com/wcrs/employee/service/EmployeeService.java
@@ -0,0 +1,13 @@
+package com.wcrs.employee.service;
+
+import com.wcrs.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+}
