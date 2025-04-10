72bcce84903f22d4fb18aa18ddf6fbd627823f6e
tendojoshua <tendojoshua@gmail.com>
2025-04-10 17:29:24 +0300

    Spring Boot Admin added

---
+++ services/wcrs-admin/src/test/java/com/wcrs/wcrs_admin/WcrsAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wcrs.wcrs_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcrsAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
