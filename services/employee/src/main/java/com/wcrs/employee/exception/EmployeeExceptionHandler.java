f77ec51ce8db2174a9f8ea7a35de910190312e38
Kibuuka Allan Victor <akibuuka50@gmail.com>
2025-04-15 16:32:37 +0300

    Added update and delete logic plus exception handling

--- services/employee/src/main/java/com/wcrs/employee/exception/EmployeeExceptionHandler.java
+++ services/employee/src/main/java/com/wcrs/employee/exception/EmployeeExceptionHandler.java
@@ -12,30 +12,39 @@ import java.util.Map;
 @RestControllerAdvice
 public class EmployeeExceptionHandler {
 
+    private static final Map<String, String> responses = new HashMap<>();
+
     @ExceptionHandler(DuplicateUserNameException.class)
     ResponseEntity<EmployeeExceptionResponse> handleDuplicateUserNameException(DuplicateUserNameException ex) {
+        responses.put("duplicate_user_name", ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new EmployeeExceptionResponse(ex.getMessage()));
+                .body(new EmployeeExceptionResponse(responses));
     }
     @ExceptionHandler(DuplicateNinException.class)
     ResponseEntity<EmployeeExceptionResponse> handleDuplicateNinException(DuplicateNinException ex) {
+        responses.put("duplicate_nin", ex.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(new EmployeeExceptionResponse(ex.getMessage()));
+                .body(new EmployeeExceptionResponse(responses));
     }
 
     @ExceptionHandler(NonExistentNINException.class)
     ResponseEntity<EmployeeExceptionResponse> handleNonExistentNINException(NonExistentNINException ex) {
+        responses.put("non_existent_nin", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new EmployeeExceptionResponse(ex.getMessage()));
+                .body(new EmployeeExceptionResponse(responses));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
-        ex.getBindingResult().getFieldErrors().forEach(error -> {
-            errors.put(error.getField(), error.getDefaultMessage());
-        });
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+   @ExceptionHandler(RequestValidationException.class)
+    ResponseEntity<EmployeeExceptionResponse> handleRequestValidationException(RequestValidationException ex) {
+        responses.put("request_validation", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new EmployeeExceptionResponse(responses));
+   }
 }
--- services/employee/src/main/java/com/wcrs/employee/repository/EmployeeRepository.java
+++ services/employee/src/main/java/com/wcrs/employee/repository/EmployeeRepository.java
@@ -4,11 +4,15 @@ import com.wcrs.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
     boolean existsByUserName(String userName);
 
     boolean existsByNIN(String nin);
 
-    Employee getEmployeeByNIN(String nin);
+    Optional<Employee> findEmployeeByNIN(String nin);
+
+    Void deleteByNIN(String nin);
 }
