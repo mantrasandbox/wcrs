e3569881e2760f589db0421748cfe2334e0dbf1c
Kibuuka Allan Victor <akibuuka50@gmail.com>
2025-04-28 14:47:58 +0300

    Added frontend react module using vite

---
+++ services/employee/src/test/java/com/wcrs/employee/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.wcrs.employee.repository;
+
+import com.wcrs.employee.model.Employee;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRepositoryTest {
+
+    private EmployeeRepository underTest;
+
+    @Test
+    void itShouldExistsByUserName() {
+        // Given
+        Employee employee = new Employee();
+
+        // When
+        // Then
+    }
+
+    @Test
+    void itShouldExistsByNIN() {
+        // Given
+        // When
+        // Then
+    }
+
+    @Test
+    void itShouldFindEmployeeByNIN() {
+        // Given
+        // When
+        // Then
+    }
+
+    @Test
+    void itShouldDeleteByNIN() {
+        // Given
+        // When
+        // Then
+    }
+}
