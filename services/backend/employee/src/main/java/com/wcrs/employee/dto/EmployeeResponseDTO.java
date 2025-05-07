05d3c26505e8cef4c2c448462090412d44e5ac30
Kibuuka Allan Victor <akibuuka50@gmail.com>
2025-05-07 16:20:56 +0300

    modified columns in emp table

--- services/backend/employee/src/main/java/com/wcrs/employee/dto/EmployeeResponseDTO.java
+++ services/backend/employee/src/main/java/com/wcrs/employee/dto/EmployeeResponseDTO.java
@@ -1,4 +1,12 @@
 package com.wcrs.employee.dto;
 
-public record EmployeeResponseDTO(String fullName, String NIN, int age) {
+public record EmployeeResponseDTO(String firstName,
+                                  String lastName,
+                                  String jobTitle,
+                                  String username,
+                                  String email,
+                                  String NIN,
+                                  PhoneDTO phone,
+                                  String gender,
+                                  int age) {
 }
