cd3265f31766c74345e16a199bb580c86b655ba7
tendojoshua <tendojoshua@gmail.com>
2025-05-08 12:13:57 +0300

    Fixed employee mapper and response dto

--- services/backend/employee/src/main/java/com/wcrs/employee/dto/EmployeeResponseDTO.java
+++ services/backend/employee/src/main/java/com/wcrs/employee/dto/EmployeeResponseDTO.java
@@ -1,12 +1,14 @@
 package com.wcrs.employee.dto;
 
+import java.util.List;
+
 public record EmployeeResponseDTO(String firstName,
                                   String lastName,
                                   String jobTitle,
                                   String username,
                                   String email,
                                   String NIN,
-                                  PhoneDTO phone,
+                                  List<PhoneDTO> phone,
                                   String gender,
                                   int age) {
 }
--- services/backend/employee/src/main/java/com/wcrs/employee/mapper/EmployeeMapper.java
+++ services/backend/employee/src/main/java/com/wcrs/employee/mapper/EmployeeMapper.java
@@ -11,7 +11,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
-import org.springframework.data.domain.Page;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,13 +22,17 @@ public interface EmployeeMapper {
     @Mapping(target = "phone", source = "phone", qualifiedByName = "wrapPhoneInList")
     Employee toEmployee(EmployeeRequestDTO employeeRequestDTO);
 
-    @Mapping(target = "fullName", expression = "java(employee.getFullName())")
+    //@Mapping(target = "fullName", expression = "java(employee.getFullName())")
     @Mapping(target = "age", expression = "java(employee.getAge())")
+    @Mapping(target = "phone", source = "phone", qualifiedByName = "toPhoneDtoList")
+    @Mapping(target = "gender", source = "gender", qualifiedByName = "genderToString")
     EmployeeResponseDTO toEmployeeResponseDTO(Employee employee);
+
     // EmployeeResponseDTO toEmployeeResponseDTO(Page employee);
 
     @Mapping(target = "phoneCategory", source = "phoneCategory", qualifiedByName = "stringToPhoneCategory")
     Phone toPhone(PhoneDTO dto);
+
 
     @Named("stringToGender")
     default Gender stringToGender(String gender) {
@@ -48,10 +51,37 @@ public interface EmployeeMapper {
         return List.of(toPhone(phoneDTO));
     }
 
+    @Named("toPhoneDto")
+    default PhoneDTO toPhoneDto(Phone phone) {
+        if (phone == null) return null;
+
+        PhoneDTO dto = new PhoneDTO();
+        dto.setNumber(phone.getNumber());
+        dto.setPhoneCategory(phone.getPhoneCategory() != null ? phoneCategoryToString(phone.getPhoneCategory()):null );
+        dto.setCountryCode(phone.getCountryCode() !=null? phone.getCountryCode().toString(): null);
+        return dto;
+    }
+
 
     @Named("stringToPhoneCategory")
     default PhoneCategory stringToPhoneCategory(String category) {
         if (category == null) return null;
         return PhoneCategory.valueOf(category.toUpperCase());
     }
+
+    @Named("phoneCategoryToString")
+    default String phoneCategoryToString(PhoneCategory category) {
+       return category !=null? category.toString():null;
+    }
+    @Named("genderToString")
+    default String genderToString(Gender gender) {
+        return gender != null? gender.toString():null;
+    }
+
+    @Named("toPhoneDtoList")
+    default List<PhoneDTO> toPhoneDtoList(List<Phone> phones) {
+        if (phones == null) return Collections.emptyList();
+        return phones.stream().map(this::toPhoneDto).toList();
+    }
+
 }
