aa3c6522fa9c739b44c495c1c571c693559c5cef
Kibuuka Allan Victor <akibuuka50@gmail.com>
2025-05-23 13:20:29 +0300

    Added Inventory mapper class

--- services/backend/inventory/src/main/java/com/wcrs/inventory/service/InventoryService.java
+++ services/backend/inventory/src/main/java/com/wcrs/inventory/service/InventoryService.java
@@ -1,9 +1,13 @@
 package com.wcrs.inventory.service;
 
+import com.wcrs.inventory.repository.MaterialRepository;
+import com.wcrs.inventory.repository.SupplierRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class InventoryService {
+    private final MaterialRepository materialRepository;
+    private final SupplierRepository supplierRepository;
 }
