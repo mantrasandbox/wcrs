657ea6013940d888645064fe6e61820e1994ce0e
tendojoshua <tendojoshua@gmail.com>
2025-05-23 16:04:03 +0300

    Added material create controller

--- services/backend/inventory/src/main/java/com/wcrs/inventory/controller/InventoryController.java
+++ services/backend/inventory/src/main/java/com/wcrs/inventory/controller/InventoryController.java
@@ -1,11 +1,15 @@
 package com.wcrs.inventory.controller;
 
+import com.wcrs.inventory.dto.MaterialRequestDTO;
+import com.wcrs.inventory.dto.MaterialResponseDTO;
 import com.wcrs.inventory.dto.SupplierRequestDTO;
 import com.wcrs.inventory.dto.SupplierResponseDTO;
+import com.wcrs.inventory.service.InventoryService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,9 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class InventoryController {
 
+    private final InventoryService inventoryService;
+
     @PostMapping("/material/create")
-    public void createMaterial(){}
+    public ResponseEntity<MaterialResponseDTO> createMaterial(@RequestBody @Valid MaterialRequestDTO materialRequestDTO){
+
+        return ResponseEntity.ok(inventoryService.createMaterial(materialRequestDTO));
+    }
 
     @PostMapping("/supplier/create")
-    public ResponseEntity<SupplierResponseDTO> createSupplier(@Valid SupplierRequestDTO supplierRequestDTO){}
+    public ResponseEntity<SupplierResponseDTO> createSupplier(@Valid SupplierRequestDTO supplierRequestDTO){
+        return ResponseEntity.ok(null);
+    }
 }
--- services/backend/inventory/src/main/java/com/wcrs/inventory/service/InventoryService.java
+++ services/backend/inventory/src/main/java/com/wcrs/inventory/service/InventoryService.java
@@ -1,7 +1,11 @@
 package com.wcrs.inventory.service;
 
+import com.wcrs.inventory.dto.MaterialRequestDTO;
+import com.wcrs.inventory.dto.MaterialResponseDTO;
+import com.wcrs.inventory.model.Material;
 import com.wcrs.inventory.repository.MaterialRepository;
 import com.wcrs.inventory.repository.SupplierRepository;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +14,11 @@ import org.springframework.stereotype.Service;
 public class InventoryService {
     private final MaterialRepository materialRepository;
     private final SupplierRepository supplierRepository;
+
+    public MaterialResponseDTO createMaterial(@Valid MaterialRequestDTO materialRequestDTO) {
+        if (materialRequestDTO != null) {
+         //Material savedMaterial =  materialRepository.saveAndFlush(materialRequestDTO)
+        }
+        return null;
+    }
 }
