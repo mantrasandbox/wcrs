5961f2c7ca8e4cf0eb4fb4b3fa9dfeb16df4efd4
tendojoshua <tendojoshua@gmail.com>
2025-05-23 13:07:25 +0300

    Inventory model and repository added

---
+++ services/backend/inventory/src/main/java/com/wcrs/inventory/model/Material.java
@@ -0,0 +1,53 @@
+package com.wcrs.inventory.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Data
+public class Material {
+
+    @Id
+    @Column(nullable = false, updatable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "material_seq")
+    @SequenceGenerator(sequenceName = "material_seq",name = "material_seq", allocationSize = 1)
+    private Integer id;
+
+    private String name;
+    private String description;
+    private String unitOfMeasure;
+    private Double costPerUnit;
+    private Double quantity;
+    private Double totalCost;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_id")
+    private Supplier supplier;
+
+
+    //Use S3 for images storage
+    //private String image;
+
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime modifiedAt;
+
+    public Double setTotalCost(){
+        return quantity * costPerUnit;
+    }
+
+}
