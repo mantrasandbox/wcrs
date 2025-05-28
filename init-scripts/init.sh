48d946bcbdc745ed3ad8f81684ad18803dd8d340
tendojoshua <tendojoshua@gmail.com>
2025-05-28 12:04:22 +0300

    Docker compose ports update

--- init-scripts/init.sh
+++ init-scripts/init.sh
@@ -3,6 +3,11 @@ set -e
 
 # This script will be executed inside postgres container on startup
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+
+    ALTER SYSTEM SET wal_level = logical;
+    CREATE USER debezium WITH REPLICATION LOGIN PASSWORD 'dbz';
+    GRANT ALL PRIVILEGES ON DATABASE postgres TO debezium;
+
     CREATE DATABASE employee;
     CREATE DATABASE notification;
     CREATE DATABASE inventory;
